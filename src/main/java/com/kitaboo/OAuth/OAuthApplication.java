8f7ca1314c830a4ad9536d346d3c7938d5b461a6
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-06 11:14:10 -0400

    todo: not sure why commented out lines 35-41 don't work

--- src/main/java/com/kitaboo/OAuth/OAuthApplication.java
+++ src/main/java/com/kitaboo/OAuth/OAuthApplication.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.KeyStore.SecretKeyEntry;
 import java.util.Scanner;
 
 import oauth.signpost.OAuthConsumer;
@@ -28,19 +29,53 @@ public class OAuthApplication {
 			String postServiceUrl = "https://cloud.kitaboo.com/DistributionServices/ext/api/registerUser";
 			String body = System.getenv("DATA");
 			String response = callPostService(postServiceUrl, System.getenv("CONSUMER_KEY"),
-					System.getenv("SECRET_KEY"), body);
+					System.getenv("SECRET_KEY"), body, "application/json; charset=utf8");
 			System.out.println(response);
 		} else if (System.getenv("ACTION").equals("purchase")) {
-			String postServiceUrl = "https://cloud.kitaboo.com/DistributionServices/ext/api/order";
-			String body = System.getenv("DATA");
-			String response = callPostService(postServiceUrl, System.getenv("CONSUMER_KEY"),
-					System.getenv("SECRET_KEY"), body);
+			// String postServiceUrl =
+			// "https://cloud.kitaboo.com/DistributionServices/ext/api/order?"
+			// + System.getenv("DATA");
+			// String response = callPostService(postServiceUrl,
+			// System.getenv("CONSUMER_KEY"),
+			// System.getenv("SECRET_KEY"), "", "application/x-www-form-urlencoded");
+			// System.out.println(response);
+			String consumerKey = System.getenv("CONSUMER_KEY");
+			String secretKey = System.getenv("SECRET_KEY");
+			String postServiceUrl = "https://cloud.kitaboo.com/DistributionServices/ext/api/order?" + System.getenv("DATA");
+			String response = new String();
+			HttpURLConnection urlConnection = null;
+			try {
+				URL url = new URL(postServiceUrl);
+				urlConnection = (HttpURLConnection) url.openConnection();
+				// Create an HttpURLConnection and add some headers
+				urlConnection.setRequestProperty("Accept", "application/json");
+				urlConnection.setRequestMethod("POST");
+				urlConnection.setReadTimeout(5 * 60 * 1000);
+				urlConnection.setDoOutput(true);
+
+				// Sign the request
+				OAuthConsumer dealabsConsumer = new DefaultOAuthConsumer(consumerKey, secretKey);
+				HttpParameters doubleEncodedParams = new HttpParameters();
+				doubleEncodedParams.put("realm", "");
+				dealabsConsumer.setAdditionalParameters(doubleEncodedParams);
+				dealabsConsumer.sign(urlConnection);
+
+				// Send the request and read the output
+				InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+				response = new Scanner(in, "UTF-8").useDelimiter("\\A").next();
+				in.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				if (null != urlConnection)
+					urlConnection.disconnect();
+			}
 			System.out.println(response);
 		} else if (System.getenv("ACTION").equals("launch_book")) {
 			String postServiceUrl = "https://cloud.kitaboo.com/DistributionServices/ext/api/LaunchBook?userID=testuser76122&bookID=14104841";
 			String body = null;
 			String response = callPostService(postServiceUrl, System.getenv("CONSUMER_KEY"),
-					System.getenv("SECRET_KEY"), body);
+					System.getenv("SECRET_KEY"), body, "application/json; charset=utf8");
 			System.out.println(response);
 		}
 	}
@@ -77,7 +112,8 @@ public class OAuthApplication {
 		return response;
 	}
 
-	public static String callPostService(String serviceEndPoint, String consumerKey, String secretKey, String payLoad) {
+	public static String callPostService(String serviceEndPoint, String consumerKey, String secretKey, String payLoad,
+			String contentType) {
 		String response = new String();
 		HttpURLConnection urlConnection = null;
 		try {
@@ -85,8 +121,7 @@ public class OAuthApplication {
 			urlConnection = (HttpURLConnection) url.openConnection();
 			// Create an HttpURLConnection and add some headers
 			urlConnection.setRequestProperty("Accept", "application/json");
-			// urlConnection.setRequestProperty("Content-Type", "application/json; charset=utf8");
-			urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=utf8");
+			urlConnection.setRequestProperty("Content-Type", contentType);
 			urlConnection.setRequestMethod("POST");
 			urlConnection.setReadTimeout(5 * 60 * 1000);
 			urlConnection.setDoOutput(true);
@@ -98,7 +133,7 @@ public class OAuthApplication {
 			dealabsConsumer.setAdditionalParameters(doubleEncodedParams);
 			dealabsConsumer.sign(urlConnection);
 
-			if (payLoad != null) {
+			if (payLoad != "") {
 				OutputStreamWriter outputStreamWriter = null;
 				try {
 					outputStreamWriter = new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8");
